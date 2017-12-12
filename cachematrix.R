23737724e497cc6fd3b2ebf8af83ba1649951440
Saurav Sengupta <sauravsengupta7@gmail.com>
2017-12-12 10:50:14 +0530

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function generates a special matrix that caches 
+## the inverse of that same matrix
 
-## Write a short comment describing this function
+## Generates a list containing the matrix, 
+## its inverse and setter/getter functions for the same
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -18,7 +19,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Generates the inverse of a matrix and stores it in the cache
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
@@ -30,11 +31,15 @@ cacheSolve <- function(x, ...) {
   data <- x$get()
   
   i <- matrixinverse(data, ...)
-  x$setmean(i)
+  x$setinverse(i)
   i
 }
 
-matrixinverse <- function(x, ...) {
+## Calculates inverse of the matrix
+## Not necessary for the assignment but I wanted to test what return() does
+## and NULL/type checks are always good programming practice ;)
+
+matrixinverse <- function(x=matrix(), ...) {
   if(!is.matrix(x)){
     message("input is not a matrix")
     return()
